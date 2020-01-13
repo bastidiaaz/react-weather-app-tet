639612c0678560f876fa887024e046300ae33272
bastidiaaz <basty.diaaz@gmail.com>
2020-01-13 18:17:04 -0300

    Added react-bootstap to project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import LocationList from './components/LocationList';
 
+import { Container, Row, Col } from 'react-bootstrap';
+import { Paper, AppBar } from 'material-ui';
 import "./App.css";
 
 const cities = [
