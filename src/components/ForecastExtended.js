1a8ae9fcfb8c13c61286b4a98ef3b43324a3dbc9
bastidiaaz <basty.diaaz@gmail.com>
2020-01-14 17:50:51 -0300

    render ForecastExtended with ForecastItem props logic

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -2,43 +2,44 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Location from './WeatherLocation/Location';
 import ForecastItem from './ForecastItem';
+import CircularProgress from "material-ui/CircularProgress";
+import { getForecastData } from './../services/weatherService';
 import './styles.css';
 
-const days = [
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-  "Sunday"
-];
-
-const data = {
-  temperature: 10,
-  weatherState: 801,
-  humidity: 10,
-  wind: '10 m/s'
-};
-
 class ForecastExtended extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      forecastData: null
+    }
+  }
+
   static propTypes = {
     city: PropTypes.string.isRequired
   };
 
+  async componentDidMount() {
+    const { city } = this.props;
+    const forecastData = await getForecastData(city);
+    this.setState({ forecastData });
+  }
+
   getForecastItemsByRange = (range) => {
     const { city } = this.props;
     return range.map((value) => {
-      return (<ForecastItem key={`${city}-${value}`} weekDay={value} hour={"13:00"} data={data}></ForecastItem>);
+      const weekDay = value.date.format('dddd DD');
+      const hour = value.date.format('HH:mm');
+      return (<ForecastItem key={`${city}-${weekDay}-${hour}`} weekDay={weekDay} hour={hour} data={value}></ForecastItem>);
     });
   };
 
   render () {
     const { city } = this.props;
+    const { forecastData } = this.state;
     return (
       <div>
         <Location city={city}></Location>
-        {this.getForecastItemsByRange(days)}
+        {(forecastData ? this.getForecastItemsByRange(forecastData) : <CircularProgress />)}
       </div>
     )
   }
