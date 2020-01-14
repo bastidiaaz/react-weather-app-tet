d1d1e650a5ac8331dcf1d6fa04ea991994c03fa3
bastidiaaz <basty.diaaz@gmail.com>
2020-01-14 17:49:39 -0300

    created getForecastData in weatherService

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 const api_key = "801b56a8784ee93dca9eaedbc7bde44e";
 
 const getWeatherData = async (location) => {
@@ -14,4 +16,22 @@ const getWeatherData = async (location) => {
   return data;
 };
 
-export default getWeatherData;
+const getForecastData = async (location) => {
+  const api_forecast = `http://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${api_key}`;
+  const rawData = await fetch(api_forecast);
+  const forecast_data = await rawData.json();
+  const data = forecast_data.list.map((value) => {
+    return {
+      temperature: Math.round(value.main.temp - 273.15),
+      weatherState: value.weather[0].id,
+      humidity: value.main.humidity,
+      wind: `${value.wind.speed} m/s`,
+      date: moment(value.dt_txt)
+    };
+  });
+
+  console.log(data);
+  return data;
+};
+
+export {getWeatherData, getForecastData};
